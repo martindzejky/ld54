038a926f4719447fec86630f63b6ad4cce84c5c7
Martin Jakubik <martindzejky@gmail.com>
2023-10-01 10:32:14 +0200

    create navigation layers for the tiles

--- scripts/door.gd
+++ scripts/door.gd
@@ -6,6 +6,8 @@ func toggle() -> void:
     if $shape.disabled:
         $shape.disabled = false
         $sprite.frame = 0
+        $navigation.enabled = false
     else:
         $shape.disabled = true
         $sprite.frame = 1
+        $navigation.enabled = true
