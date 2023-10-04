13788abd07b36bfcebdd8de0ef0a69cefb8f4657
Martin Jakubik <martindzejky@gmail.com>
2023-10-04 23:00:13 +0200

    make customers navigate below the cash desk when paying
    fixes #1

--- scripts/ai/state-cash-desk-pay.gd
+++ scripts/ai/state-cash-desk-pay.gd
@@ -7,52 +7,52 @@ var cashDeskComputer: Node
 
 
 func enter():
-    
+
     cashDeskComputer = get_tree().get_first_node_in_group('cash-desk-computer')
     assert(cashDeskComputer, 'Missing cash desk computer!')
-    
-    customer.targetPosition = cashDeskComputer.global_position
-    agent.target_position = cashDeskComputer.global_position
-    
+
+    customer.targetPosition = cashDeskComputer.global_position + Vector2(0, 16)
+    agent.target_position = customer.targetPosition
+
     animation.play('walk')
-    
+
 func exit():
-    
+
     customer.targetPosition = customer.global_position
     agent.target_position = customer.targetPosition
     agent.velocity = Vector2.ZERO
-    
+
     animation.play('idle')
 
 func physicsUpdate(_delta):
-    
+
     if agent.is_navigation_finished() or agent.distance_to_target() < 20:
-        
+
         print('Paying at the cash desk')
         customer.displayCallout(3)
-        
-        
+
+
         var products := get_tree().get_nodes_in_group('product')
         var myProducts := products.filter(func (product): return product.customer == customer)
         var total := 0.0
-        
+
         for product in myProducts:
             total += product.price
-        
+
         var price := floatingPrice.instantiate()
         assert(price is FloatingPrice, 'Not a floating price!')
         price.setPrice(total)
         get_node('/root').add_child(price)
-        
+
         Shop.addSoldAmount(total)
         Sfx.play('sell')
-        
+
         emit_signal('transition', 'fetch-paid-products')
         return
-    
+
     var nextPathPosition := agent.get_next_path_position()
     var newVelocity := nextPathPosition - customer.global_position
-    
+
     newVelocity = newVelocity.normalized()
     newVelocity = newVelocity * customer.walkSpeed
 
--- scripts/customer.gd
+++ scripts/customer.gd
@@ -19,23 +19,23 @@ func _init():
     call_deferred('setupColor')
 
 func setupColor():
-    
+
     # randomize the shirt color
     var randomColor := Color.from_hsv(randf(), 0.8, 0.8)
-    
+
     $sprite.material.set_shader_parameter('modulate', randomColor)
     $"sprite-arms".material.set_shader_parameter('modulate', randomColor)
 
 
 func _ready():
-    
+
     $agent.max_speed = walkSpeed
-    
+
     # choose products that the customer wants
-    
+
     var catalogProducts = Catalog.get_children()
     assert(catalogProducts.size() > 0, 'The catalog is empty!')
-    
+
     for i in range(randi_range(1, 4)):
         var product = catalogProducts.pick_random()
         assert(product is Product, 'The catalog contains a node which is not a Product!')
@@ -43,16 +43,16 @@ func _ready():
 
 
 func displayCallout(index: int, product: Product = null):
-    
+
     $"ui-timer".stop()
     $ui.visible = true
     Sfx.play('callout')
-    
+
     $ui/margin/center/texture.texture.region = Rect2(index * 16, 0, 16, 16)
-    
+
     if not product:
         $"ui/margin/center/product-texture".visible = false
-        
+
     else:
         $"ui/margin/center/product-texture".visible = true
         $"ui/margin/center/product-texture".texture = AtlasTexture.new()
@@ -63,7 +63,7 @@ func displayCallout(index: int, product: Product = null):
             16.0,
             16.0
         )
-    
+
     $"ui-timer".start()
 
 func hideCallout():
