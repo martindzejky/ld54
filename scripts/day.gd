4777963fc101781b5c4b8d865f295e854ccd4360
Martin Jakubik <martindzejky@gmail.com>
2023-10-04 23:18:00 +0200

    pixel art day label

--- scripts/day.gd
+++ scripts/day.gd
@@ -2,12 +2,17 @@ extends Control
 
 
 func _ready():
-    
+
     Shop.soldToday = 0.0
     Shop.isOpen = true
     Sfx.play('day')
-    
+
     $margin/center/label.text = 'Day ' + var_to_str(Shop.day)
+
+    await get_tree().process_frame
+
+    $margin/center/label.pivot_offset = $margin/center/label.size / 2
+    $margin/center/label.scale = Vector2(6, 6)
 
 
 func _on_timer_timeout():
