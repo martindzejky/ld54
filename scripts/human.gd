e5310048300cec5272307862e78d98edc367058e
Martin Jakubik <martindzejky@gmail.com>
2023-10-04 23:37:30 +0200

    calculate human facing and drop items in front
    Makes customers less likely to place basket right under them and get
    stuck.

--- scripts/human.gd
+++ scripts/human.gd
@@ -4,6 +4,18 @@ class_name Human
 
 @export var walkSpeed = 30.0
 
+var previousPosition = position
+
+
+func _process(_delta):
+
+    if previousPosition.distance_squared_to(position) < .1: return
+
+    var facingDirection = previousPosition.direction_to(position)
+    $facing.position = facingDirection * 12.0
+
+    previousPosition = position
+
 
 
 func isCarryingAnything() -> bool:
@@ -13,28 +25,28 @@ func pickItem(item: Node2D, force = false):
     if isCarryingAnything() and not force: return
     $"sprite-arms".visible = true
     Sfx.play('pick')
-    
+
     item.reparent($hands, false)
     item.position = Vector2.ZERO
     item.rotation = 0
-    
+
     if item.has_method('_on_picked_up'):
         item.call('_on_picked_up')
-    
+
 func dropItem() -> Node2D:
     if not isCarryingAnything(): return null
     $"sprite-arms".visible = false
     Sfx.play('drop')
-    
+
     for carriedItem in $hands.get_children():
         var level := get_tree().get_first_node_in_group('level')
         if level:
             carriedItem.reparent(level)
-            carriedItem.global_position = global_position
-            
+            carriedItem.global_position = $facing.global_position
+
             if carriedItem.has_method('_on_dropped'):
                 carriedItem.call('_on_dropped')
-            
+
             return carriedItem
-    
+
     return null
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,8 +4,8 @@ class_name Player
 
 
 func _physics_process(_delta):
-    
-    
+
+
     # if any GUI is focused, ignore move inputs
     if get_viewport().gui_get_focus_owner():
         $animation.play('thinking')
@@ -14,83 +14,83 @@ func _physics_process(_delta):
     var moveX := Input.get_axis("move_left", "move_right")
     var moveY := Input.get_axis("move_up", "move_down")
     var moveInput := Vector2(moveX, moveY).normalized()
-    
+
     if moveInput.length_squared() > 0:
-        $"interaction-area".position = Vector2(0, -2.0) + moveInput * Vector2(5.0, 4.0)
         $animation.play('walk')
-    
     else:
         $animation.play('idle')
-    
+
     velocity = moveInput * walkSpeed
     move_and_slide()
 
-func _process(_delta):
-    
+func _process(delta):
+
+    super(delta)
+
     # if any GUI is focused, ignore move inputs
     if get_viewport().gui_get_focus_owner(): return
-    
+
     if Input.is_action_just_pressed("interact"):
         interact()
-        
+
 
 func sortingFunction(a, b):
-    
+
     # always prefer baskets
     if b is Basket: return false
     if a is Basket: return true
-    
+
     # prefer interacting with containers first
     if b is ItemContainer: return false
     if a is ItemContainer: return true
-    
+
     # sort by distance
     return global_position.distance_squared_to(a.global_position) < global_position.distance_squared_to(b.global_position)
 
 
 func interact():
-    
-    var overlappingBodies := $"interaction-area".get_overlapping_bodies() as Array[Node2D]
-    var overlappingAreas := $"interaction-area".get_overlapping_areas() as Array[Area2D]
-    
+
+    var overlappingBodies := $"facing/interaction-area".get_overlapping_bodies() as Array[Node2D]
+    var overlappingAreas := $"facing/interaction-area".get_overlapping_areas() as Array[Area2D]
+
     overlappingBodies.append_array(overlappingAreas)
     overlappingBodies.sort_custom(sortingFunction)
-    
+
     # interact with the first interactible overlapping body
     for body in overlappingBodies:
-        
+
         if body.is_in_group('door'):
             (body as Door).toggle()
             return
         if body.is_in_group('door-area'):
             (body.get_parent() as Door).toggle()
             return
-        
+
         if body is CashDeskComputer:
-            
+
             if isCarryingAnything():
-                
+
                 var item := dropItem()
                 if item is Product and item.customer:
                     item.isScanned = true
                     item.get_node('scanned-check').visible = true
                     body.scanItem()
                 pickItem(item)
-                
+
             else:
                 body.openUI()
                 body.focusUI()
-            
+
             return
-        
+
         if body is ItemContainer:
             var container := body as ItemContainer
-            
+
             if isCarryingAnything():
                 if container.canStoreItem():
                     var item := dropItem()
                     if item:
-                        
+
                         # make sure that if carrying a basket, it cannot be inserted
                         # (unless this is a cash desk)
                         if item is ItemContainer and not container.is_in_group('cash-desk'):
@@ -99,7 +99,7 @@ func interact():
                         else:
                             container.storeItem(item)
                             return
-                        
+
             else:
                 if container.capacity > 1:
                     container.openUI()
@@ -110,12 +110,12 @@ func interact():
                     if item:
                         pickItem(item)
                         return
-        
+
         if body.is_in_group('pickable') and not isCarryingAnything():
             pickItem(body)
             return
-            
-    
+
+
     # if nothing can be interacted with, the player can drop the item they are carrying
     if isCarryingAnything():
         dropItem()
