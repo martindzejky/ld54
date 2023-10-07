b94af831343f1796eec027ae193d81852c08e5f6
Martin Jakubik <martindzejky@gmail.com>
2023-10-07 22:57:49 +0200

    try to improve the navigation

--- scripts/basket.gd
+++ scripts/basket.gd
@@ -9,24 +9,20 @@ var customer: Customer
 func _on_picked_up():
     $shadow.visible = false
     $shape.disabled = true
-    $obstacle.avoidance_enabled = false
-    $obstacle2.avoidance_enabled = false
+    #$obstacle.avoidance_enabled = false
 
 func _on_stored():
     $shadow.visible = false
     #$shape.disabled = true # has to be enabled for cash desk interaction
-    $obstacle.avoidance_enabled = false
-    $obstacle2.avoidance_enabled = false
+    #$obstacle.avoidance_enabled = false
 
 
 func _on_dropped():
     $shadow.visible = true
     $shape.disabled = false
-    $obstacle.avoidance_enabled = true
-    $obstacle2.avoidance_enabled = true
+    #$obstacle.avoidance_enabled = true
 
 func _on_retrieved():
     $shadow.visible = true
     $shape.disabled = false
-    $obstacle.avoidance_enabled = true
-    $obstacle2.avoidance_enabled = true
+    #$obstacle.avoidance_enabled = true
