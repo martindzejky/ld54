1eedfe53d591367f06e7a4b2d4b177ef02b26a7d
Martin Jakubik <martindzejky@gmail.com>
2023-10-09 10:33:08 +0200

    throw away my procedural navigation mesh, it does not work :shrug:

--- scripts/ai/state-idle.gd
+++ scripts/ai/state-idle.gd
@@ -10,10 +10,6 @@ func enter():
     # wait for a random amount of time
     await get_tree().create_timer(randf_range(2.0, 5.0)).timeout
 
-    # TODO: for testing...
-    emit_signal('transition', 'wander')
-    return
-
     if randf() < 0.2:
         emit_signal('transition', 'wander')
     else:
