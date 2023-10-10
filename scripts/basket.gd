905600ebd6d42fb6c33111be5576c1f621c15af5
Martin Jakubik <martindzejky@gmail.com>
2023-10-10 12:44:30 +0200

    simplify collisions massively
    - Everything has a circle collision shape
    - No avoidance obstacles for baskets, customers will just push them away
    
    closes #5

--- scripts/basket.gd
+++ scripts/basket.gd
@@ -9,22 +9,16 @@ var customer: Customer
 func _on_picked_up():
     $shadow.visible = false
     $shape.disabled = true
-    $obstacle.avoidance_enabled = false
-    get_tree().call_deferred('call_group', 'navigation', 'update')
 
 func _on_stored():
     $shadow.visible = false
     #$shape.disabled = true # has to be enabled for cash desk interaction
-    $obstacle.avoidance_enabled = false
 
 
 func _on_dropped():
     $shadow.visible = true
     $shape.disabled = false
-    $obstacle.avoidance_enabled = true
-    get_tree().call_deferred('call_group', 'navigation', 'update')
 
 func _on_retrieved():
     $shadow.visible = true
     $shape.disabled = false
-    $obstacle.avoidance_enabled = true
